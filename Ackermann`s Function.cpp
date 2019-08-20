cffadc9a29cba6189fe96cdc7f85a4430ccb8eb2
Ali Faisal <alifaisalas99@gmail.com>
2019-08-20 13:10:42 +0500

    beta2

--- Ackermann`s Function.cpp
+++ Ackermann`s Function.cpp
@@ -1,11 +1,15 @@
+// Author: Ali Faisal
+// A(4, 1) is limit
+
 #include <iostream>
+#define y 100000
 
 using namespace std;
 
 int ackermann(int m, int n);
 void clearCache();
 void printCache();
-int cache[5][6] = {0};
+int cache[5][y] = {0};
 
 int main()
 {
@@ -15,12 +19,12 @@ int main()
     
     for (m = 0; m <= 4; m++)
     {
-    	for (n = 0; n < 6 - m; n++)
+    	for (n = 0; n < 6-m; n++)
         {
         	cache[0][0] = 1;
         	printf("A(%d, %d) = %d\n", m, n, ackermann(m, n));
-        	printCache();
-			clearCache();
+        	//printCache();
+			//clearCache();
 		}
 	}
 	
@@ -37,25 +41,32 @@ int ackermann(int m, int n)
     else if(m > 0 && n == 0)
     {
     	// A(m-1, 1)
-    	if(cache[m-1][1] != -1)
+    	if(cache[m][n] != -1)
     	{
-    		return cache[m-1][1];
+    		return cache[m][n];
 		}
 		else
 		{
-			return cache[m-1][1] = ackermann(m-1, 1);
+			if(cache[m-1][1] != -1)
+			{
+				return cache[m][n] = cache[m-1][1];
+			}
+			else
+			{
+				return cache[m][n] = ackermann(m-1, 1);
+			}
 		}
 	}
 	else if(m > 0 && n > 0)
 	{
 		// A(m - 1, A(m, n - 1));
-		if(cache[m][n-1] != -1)
+		if(cache[m][n] != -1)
 		{
-			return ackermann(m - 1, cache[m][n-1]);
+			return cache[m][n];
 		}
 		else
 		{
-			return ackermann(m - 1, ackermann(m, n - 1));
+			return cache[m][n] = ackermann(m - 1, ackermann(m, n - 1));
 		}
 	}
 }
@@ -64,7 +75,7 @@ void clearCache()
 {
 	for(int i=0; i<5; i++)
     {
-    	for(int j=0; j<6; j++)
+    	for(int j=0; j<y; j++)
     	{
     		cache[i][j] = -1;
 		}
